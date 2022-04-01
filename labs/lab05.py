12e90fc042e270f07381465fea3f412994251264
Jordan <jordan@vieler.org>
2022-04-01 10:37:20 -0600

    completed lab05(06)

---
+++ labs/lab05.py
@@ -0,0 +1,92 @@
+this_file = __file__
+
+
+def make_adder_inc(a):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2)
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    i = 0
+
+    def adder_inc(b):
+        nonlocal i
+        out = a + b + i
+        i += 1
+        return out
+
+    return adder_inc
+
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    fib_prev = 0
+    fib_curr = 0
+    fib_next = 1
+
+    def fib():
+        nonlocal fib_prev
+        nonlocal fib_curr
+        nonlocal fib_next
+        out = fib_curr
+        fib_prev = fib_curr
+        fib_curr = fib_next
+        fib_next += fib_prev
+        return out
+
+    return fib
+
+
+def insert_items(lst, entry, elem):
+    """
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    i = 0
+    while i < len(lst):
+        if lst[i] == entry:
+            i += 1
+            lst.insert(i, elem)
+        i += 1
+    return lst
